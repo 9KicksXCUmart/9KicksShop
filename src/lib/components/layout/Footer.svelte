c0c2cab339de1bbb737b8fac36ab1036f7709d2b
nickwkt <nickb55245@gmail.com>
2024-03-08 23:44:17 +0800

    fix: center the footer and left align the text

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -5,29 +5,37 @@
 <footer class="bg-fgray text-black py-6">
 	<div class="container mx-auto flex flex-wrap justify-center">
 		<!-- Navigation -->
-		<div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 mb-4 sm:mb-0">
-			<h3 class=" font-semibold mb-2">Navigation</h3>
-			<ul>
-				<li><a href="/" class="hover:text-neutral-300 text-sm">Home</a></li>
-				<li><a href="/products" class="hover:text-neutral-300 text-sm">Products</a></li>
-			</ul>
+		<div class="w-full sm:w-1/2 md:w-1/4 mb-4 sm:mb-0">
+			<div class="flex flex-col justify-start items-center">
+				<div class="text-left">
+					<div class="font-semibold mb-2">Navigation</div>
+					<div><a href="/" class="hover:text-neutral-300 text-sm">Home</a></div>
+					<div><a href="/products" class="hover:text-neutral-300 text-sm">Products</a></div>
+				</div>
+			</div>
 		</div>
 
 		<!-- Contact Information -->
-		<div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 mb-4 sm:mb-0">
-			<h3 class=" font-semibold mb-2">Contact Us</h3>
-			<p>Email: 9kicks-shop@gmail.com</p>
+		<div class="w-full sm:w-1/2 md:w-1/4 mb-4 sm:mb-0">
+			<div class="flex flex-col justify-start items-center">
+				<div class="text-left">
+					<h3 class="font-semibold mb-2">Contact Us</h3>
+					<p>Email: 9kicks-shop@gmail.com</p>
+				</div>
+			</div>
 		</div>
 
 		<!-- Social Media Links -->
-		<div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
-			<h3 class=" font-semibold mb-2">Connect With Us</h3>
-			<div class="flex space-x-4">
-				<!-- <a href="#" class="text-neutral-800 hover:text-neutral-300"><i class="fab fa-facebook"></i></a>
-		  <a href="#" class="text-neutral-800 hover:text-neutral-300"><i class="fab fa-twitter"></i></a> -->
-				<a href="https://www.instagram.com" class="text-neutral-800 hover:text-neutral-300">
-					<Instagram class="w-4 h-4" />
-				</a>
+		<div class="w-full sm:w-1/2 md:w-1/4 mb-4 sm:mb-0">
+			<div class="flex flex-col justify-start items-center">
+				<div class="text-left">
+					<h3 class=" font-semibold mb-2">Connect With Us</h3>
+					<div class="flex space-x-4">
+						<a href="https://www.instagram.com" class="text-neutral-800 hover:text-neutral-300">
+							<Instagram class="w-4 h-4" />
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
