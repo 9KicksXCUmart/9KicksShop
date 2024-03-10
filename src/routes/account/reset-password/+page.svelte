c8cc96eb80c29823f5f864482f1f0d8ca3033543
nickwkt <nickb55245@gmail.com>
2024-03-10 19:15:40 +0800

    feat: add reset password page

---
+++ src/routes/account/reset-password/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import ResetForm from './reset-form.svelte';
+
+	export let data: PageData;
+</script>
+
+<div class="flex flex-col items-center justify-center gap-5 my-5">
+	<div class="flex justify-start">
+		<div class="text-3xl font-bold">Reset Password</div>
+	</div>
+	<div class="mt-4">
+		<ResetForm data={data.form} />
+	</div>
+</div>
