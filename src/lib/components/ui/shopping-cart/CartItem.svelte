36b14e332c4ac3e96afbf6fe83f6c08af20eab63
felixfung203 <felixlam203@gmail.com>
2024-03-30 15:05:30 +0800

    feat: merge shoppingCart api

--- src/lib/components/ui/shopping-cart/CartItem.svelte
+++ src/lib/components/ui/shopping-cart/CartItem.svelte
@@ -1,25 +1,28 @@
-<script>
-	export let productImage;
-	export let productName;
-	export let gender;
-	export let price;
-	export let size;
-	export let quantity;
-	export let id;
+<script lang="ts">
+	export let productImage: string;
+	export let productName: string;
+	export let productCategory: string;
+	export let price: number;
+	export let size: string;
+	export let quantity: number;
+	export let id: string;
 </script>
 
 <!-- ShoppingCartItem -->
-<div id={id} class="relative flex flex-row items-center w-[775px] px-[17px] space-x-[75px] bg-[#f8fbf6]">
+<div
+	{id}
+	class="relative flex flex-row items-center w-[775px] px-[17px] space-x-[75px] bg-[#f8fbf6]"
+>
 	<!-- ShoppingCartItem/Image -->
 	<div>
-		<img src={productImage} alt="Product" class="w-[290px] h-[270px]"/>
+		<img src={productImage} alt="Product" class="w-[290px] h-[270px]" />
 	</div>
 	<!-- ShoppingCartItem/CartInfo -->
 	<div class="flex flex-col w-[370px] space-y-[16px]">
 		<div class="flex flex-row w-[260px] place-content-between">
 			<div class="flex flex-col space-y-[8px]">
 				<span class="text-xl font-bold"> {productName} </span>
-				<span class="text-lg"> {gender} </span>
+				<span class="text-lg"> {productCategory} </span>
 			</div>
 			<span class="text-lg">US${price}</span>
 		</div>
@@ -37,6 +40,18 @@
 	</div>
 	<!-- ShoppingCartItem/DeleteItemButton-->
 	<button class="absolute top-[19px] right-[20px]">
-		<svg class="hover:stroke-[#6d7d7d]" xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="arcs"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+		<svg
+			class="hover:stroke-[#6d7d7d]"
+			xmlns="http://www.w3.org/2000/svg"
+			width="33"
+			height="33"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="#000000"
+			stroke-width="1.5"
+			stroke-linecap="square"
+			stroke-linejoin="arcs"
+			><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg
+		>
 	</button>
 </div>
--- src/routes/shopping-cart/+page.svelte
+++ src/routes/shopping-cart/+page.svelte
@@ -3,17 +3,46 @@
 	import TextBanner from '$lib/components/ui/banner/PageHeader.svelte';
 	import CartItem from '$lib/components/ui/shopping-cart/CartItem.svelte';
 	import SummaryPanel from '$lib/components/ui/shopping-cart/Summary.svelte';
+	import { onMount } from 'svelte';
+
+	let itemData = {};
+	onMount(async () => {
+		itemData = await getShoppingCartItems();
+	});
+
+	async function getShoppingCartItems() {
+		const response = await fetch(
+			'http://localhost:8080/api/v1/shopping-cart?' +
+				new URLSearchParams({ userId: '120499e3-fdfd-440c-1204-bdcd954f4891' }).toString(),
+			{
+				method: 'GET',
+				headers: { 'Content-Type': 'application/json' }
+			}
+		);
+		const result = await response.json();
+		return result.data.shoppingCartItemDetail;
+	}
 </script>
 
 <div class="flex flex-col items-center w-full h-fit">
-	<TextBanner text="Shopping Cart"/>
+	<TextBanner text="Shopping Cart" />
 	<div class="flex flex-row justify-between items-start w-full h-fit px-[17%] py-[1%] space-x-[6%]">
 		<!-- Cart Item List -->
-		<div class="w-[40%] h-[1300px]">
-			<CartItem productImage={ProductImage}/>
-		</div>
+		{#each Object.entries(itemData) as [shoppingCartId, itemsDetail]}
+			<div class="w-[40%] h-[1300px]">
+				<CartItem
+					productImage={itemsDetail.imageUrl}
+					price={itemsDetail.price}
+					productName={itemsDetail.productName}
+					productCategory={itemsDetail.productCategory}
+					size={itemsDetail.productSize}
+					quantity={itemsDetail.productQuantity}
+					id={shoppingCartId}
+				/>
+			</div>
+		{/each}
 		<!-- Summary -->
-		<SummaryPanel/>	
+		<SummaryPanel />
 	</div>
 </div>
 
