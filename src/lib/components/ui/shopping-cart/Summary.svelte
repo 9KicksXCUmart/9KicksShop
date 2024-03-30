145de5384d1c38f0919ad044a5cbfb42ef601268
felixfung203 <felixlam203@gmail.com>
2024-03-31 01:02:00 +0800

    feat: merge shoppingCart api with page

--- src/lib/components/ui/shopping-cart/Summary.svelte
+++ src/lib/components/ui/shopping-cart/Summary.svelte
@@ -1,15 +1,14 @@
-<script>
+<script lang="ts">
 	import RightArrowButton from '$lib/components/ui/button/RightArrowButton.svelte';
-	export let id;
-	export let totalItemPrice;
-	export let itemCount;
-	export let shippingFee;
-	export let totalPrice;
+	export let totalItemPrice: number;
+	export let itemCount: number;
+	export let shippingFee: number;
+	export let totalPrice: number;
+	export let discount: number;
 </script>
 
-
 <!-- ShoppingCartSummary -->
-<div id={id} class="relative flex flex-col w-[411px] h-fit p-[20px] space-y-[15px] bg-[#f8fbf6]">
+<div class="relative flex flex-col w-[411px] h-fit p-[20px] space-y-[15px] bg-[#f8fbf6]">
 	<div class="flex pb-[10px]">
 		<span class="text-3xl font-extrabold"> Order Summary </span>
 	</div>
@@ -20,18 +19,26 @@
 	<div>
 		<span class="text-base text-[#7d7d7d]"> {itemCount} item(s) </span>
 	</div>
-	<hr>
+	<hr />
 	<div class="flex flex-row w-full place-content-between">
 		<span class="text-xl font-bold"> Shipping </span>
-		<span class="text-lg"> {shippingFee} </span>
+		{#if shippingFee}
+			<span class="text-lg"> {shippingFee} </span>
+		{:else}
+			<span class="text-lg">TBC</span>
+		{/if}
 	</div>
-	<hr>
+	<hr />
+	<div class="flex flex-row w-full place-content-between">
+		<span class="text-xl font-bold"> Discount </span>
+		<span class="text-lg"> {discount} </span>
+	</div>
+	<hr />
 	<div class="flex flex-row w-full place-content-between">
 		<span class="text-xl font-bold"> Estimated Total </span>
 		<span class="text-lg"> US${totalPrice} </span>
 	</div>
 	<div class="flex flex-row w-full place-content-center py-[20px]">
-		<RightArrowButton text="Checkout" scale="0.85"/>
+		<RightArrowButton text="Checkout" scale="0.85" />
 	</div>
-
 </div>
--- src/routes/shopping-cart/+page.svelte
+++ src/routes/shopping-cart/+page.svelte
@@ -1,13 +1,27 @@
-<script>
+<script lang="ts">
 	import ProductImage from '$lib/client/images/dunklow_bw_preview.png';
 	import TextBanner from '$lib/components/ui/banner/PageHeader.svelte';
 	import CartItem from '$lib/components/ui/shopping-cart/CartItem.svelte';
 	import SummaryPanel from '$lib/components/ui/shopping-cart/Summary.svelte';
 	import { onMount } from 'svelte';
 
-	let itemData = {};
+	interface shoppingCartItemDetail {
+		shoppingCartItemDetail: {
+			imageUrl: string;
+			price: number;
+			productName: string;
+			productCategory: string;
+			productSize: string;
+			productQuantity: number;
+		};
+	}
+
+	let itemDatas = {} as shoppingCartItemDetail;
+	let orderSummaryData: any;
+
 	onMount(async () => {
-		itemData = await getShoppingCartItems();
+		orderSummaryData = await getOrderSummary();
+		itemDatas = await getShoppingCartItems();
 	});
 
 	async function getShoppingCartItems() {
@@ -22,27 +36,51 @@
 		const result = await response.json();
 		return result.data.shoppingCartItemDetail;
 	}
+
+	async function getOrderSummary() {
+		const response = await fetch(
+			'http://localhost:8080/api/v1/payment/order-summary?' +
+				new URLSearchParams({ userId: '120499e3-fdfd-440c-1204-bdcd954f4891' }).toString(),
+			{
+				method: 'GET',
+				headers: { 'Content-Type': 'application/json' }
+			}
+		);
+		const result = await response.json();
+		return result.data;
+	}
 </script>
 
 <div class="flex flex-col items-center w-full h-fit">
-	<TextBanner text="Shopping Cart" />
-	<div class="flex flex-row justify-between items-start w-full h-fit px-[17%] py-[1%] space-x-[6%]">
-		<!-- Cart Item List -->
-		{#each Object.entries(itemData) as [shoppingCartId, itemsDetail]}
-			<div class="w-[40%] h-[1300px]">
-				<CartItem
-					productImage={itemsDetail.imageUrl}
-					price={itemsDetail.price}
-					productName={itemsDetail.productName}
-					productCategory={itemsDetail.productCategory}
-					size={itemsDetail.productSize}
-					quantity={itemsDetail.productQuantity}
-					id={shoppingCartId}
-				/>
+	<div class="flex flex-col items-center w-[1920px] h-fit">
+		<TextBanner text="Shopping Cart" />
+		<div
+			class="flex flex-row justify-between items-start w-full h-fit px-[17%] py-[1%] space-x-[6%]"
+		>
+			<!-- Cart Item List -->
+			<div class="flex flex-col space-y-[15px] w-[40%] h-[1300px]">
+				{#each Object.entries(itemDatas) as [id, itemDetail]}
+					<CartItem
+						productImage={itemDetail.imageUrl}
+						price={itemDetail.price}
+						productName={itemDetail.productName}
+						productCategory={itemDetail.productCategory}
+						size={itemDetail.productSize}
+						quantity={itemDetail.productQuantity}
+						{id}
+					/>
+				{/each}
 			</div>
-		{/each}
-		<!-- Summary -->
-		<SummaryPanel />
+			{#if orderSummaryData}
+				<SummaryPanel
+					totalPrice={orderSummaryData.actualPrice}
+					itemCount={orderSummaryData.itemCount}
+					shippingFee={orderSummaryData.shippingFee}
+					totalItemPrice={orderSummaryData.totalPrice}
+					discount={orderSummaryData.discount}
+				/>
+			{/if}
+		</div>
 	</div>
 </div>
 
