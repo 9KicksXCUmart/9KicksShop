d800dc81e24df4eb2d0d43007b346ea89fb10419
LilAiluropoda <bcheng8798@gmail.com>
2024-03-30 14:55:42 +0800

    fix: add flex prop to CartItem list

--- src/routes/shopping-cart/+page.svelte
+++ src/routes/shopping-cart/+page.svelte
@@ -6,17 +6,20 @@
 </script>
 
 <div class="flex flex-col items-center w-full h-fit">
-	<TextBanner text="Shopping Cart"/>
-	<div class="flex flex-row justify-between items-start w-full h-fit px-[17%] py-[1%] space-x-[6%]">
-		<!-- Cart Item List -->
-		<div class="w-[40%] h-[1300px]">
-			<CartItem productImage={ProductImage}/>
+	<div class="flex flex-col items-center w-[1920px] h-fit">
+		<TextBanner text="Shopping Cart"/>
+		<div class="flex flex-row justify-between items-start w-full h-fit px-[17%] py-[1%] space-x-[6%]">
+			<!-- Cart Item List -->
+			<div class="flex flex-col space-y-[15px] w-[40%] h-[1300px]">
+				<CartItem productImage={ProductImage}/>
+
+				<CartItem productImage={ProductImage}/>
+			</div>
+			<!-- Summary -->
+			<SummaryPanel/>	
 		</div>
-		<!-- Summary -->
-		<SummaryPanel/>	
 	</div>
 </div>
-
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.white);
