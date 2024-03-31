253ba00176b38cd66ee6108d3f87b38d331f5851
LilAiluropoda <bcheng8798@gmail.com>
2024-03-31 16:22:31 +0800

    fix: removed svelte.config.js code for svelte-carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,27 @@
 <script>
+	import Carousel from 'svelte-carousel';
+	import { browser } from '$app/environment';
+
+	let carousel;
+
+	const handleNextClick = () => {
+		carousel.goToNext()
+	}
 </script>
 
-<h1>hello</h1>
+<div class="flex flex-col items-center w-full h-full">
+	<!-- 1920x1080 Container -->
+	<div class="flex flex-col items-center w-[1920px] h-fit">
+		{#if browser}
+			<Carousel bind:this={carousel}>
+				<div>1</div>
+				<div>2</div>
+				<div>3</div>
+			</Carousel>
+		{/if}
+		<button on:click={handleNextClick}>Next</button>
+	</div>
+</div>
 
 <style lang="postcss">
 	:global(html) {
--- svelte.config.js
+++ svelte.config.js
@@ -13,12 +13,6 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 
-		// vite support added for svelte-carousel
-		vite: {
-			optimizeDeps: {
-				include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
-			}
-		}
 	}
 };
 
