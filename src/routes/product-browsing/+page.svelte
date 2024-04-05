054ad8c6d8a0ef479cc540d53754e7ea531c981f
LilAiluropoda <bcheng8798@gmail.com>
2024-04-05 23:40:37 +0800

    chore: fix MenuItemButton.svelte and format code

---
+++ src/routes/product-browsing/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<div class="flex flex-col items-center w-full h-fit">
+	<div class="flex flex-col items-center w-[1920px] h-fit">
+		<TextBanner text="Product Browsing" />
+		<div
+			class="flex flex-row justify-between items-start w-full h-fit px-[17%] py-[1%] space-x-[6%]"
+		></div>
+	</div>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.white);
+	}
+</style>
