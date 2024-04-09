17a0c6b394a0e5c6c00061d2e90b3036b49de4e4
Colin Lo <69583542+locolin1204@users.noreply.github.com>
2024-04-10 04:25:29 +0800

    feat: implement product detail page (#27)
    * feat: create dynamic routing for product detail page
    
    * feat: finish API integration of product detail.
    
    * fix: fix css component and hardcodes
    
    * style: fix styling
    
    ---------
    
    Co-authored-by: LilAiluropoda <bcheng8798@gmail.com>

--- src/lib/components/ui/product/InfoPanel.svelte
+++ src/lib/components/ui/product/InfoPanel.svelte
@@ -7,41 +7,56 @@
 	export let quantitys;
 	export let rating;
 	export let price;
+	export let isDiscount;
+	export let discountPrice;
+
+	export let selectedSize;
+	export let selectedQuantity;
 </script>
 
 <!-- InfoPanel-->
-<div class="flex flex-col w-[400px] h-[360px] space-y-[12px] p-[20px] bg-[#f8fbf6]">
-	<!-- InfoPanel/Price -->
-	<span style="font-size:40px"> US ${price}</span>
-	<!-- InfoPanel/Rating -->
-	<div class="flex flex-row place-items-center space-x-[2px]">
-		{#each { length: rating } as _, i}
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20">
-				<path
-					d="M10 0l2.47 7.55h8.02l-6.47 4.7 2.47 7.55L10 15.1l-6.47 4.7 2.47-7.55L0 7.55h8.02L10 0z"
-					fill="black"
-				/>
-			</svg>
-		{/each}
-		{#each { length: 5 - rating } as _, i}
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20">
-				<path
-					d="M10 0l2.47 7.55h8.02l-6.47 4.7 2.47 7.55L10 15.1l-6.47 4.7 2.47-7.55L0 7.55h8.02L10 0z"
-					fill="#d9d9d9"
-				/>
-			</svg>
-		{/each}
+<div class="flex flex-col w-[400px] h-fit space-y-[12px] p-6 bg-[#f8fbf6] items-center pt-10 pb-20">
+	<div class="flex flex-col items-start w-full">
+		{#if !isDiscount}
+			<!-- InfoPanel/DiscountPrice -->
+			<div class="flex flex-col">
+				<span class="text-4xl"> US ${200}</span>
+				<span class="line-through text-gray-400 text-2xl"> US ${price}</span>
+			</div>
+		{:else}
+			<!-- InfoPanel/Price -->
+			<span class="text-4xl"> US ${price}</span>
+		{/if}
+		<!-- InfoPanel/Rating -->
+		<div class="flex flex-row place-items-center space-x-[2px]">
+			{#each { length: rating } as _, i}
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20">
+					<path
+						d="M10 0l2.47 7.55h8.02l-6.47 4.7 2.47 7.55L10 15.1l-6.47 4.7 2.47-7.55L0 7.55h8.02L10 0z"
+						fill="black"
+					/>
+				</svg>
+			{/each}
+			{#each { length: 5 - rating } as _, i}
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20">
+					<path
+						d="M10 0l2.47 7.55h8.02l-6.47 4.7 2.47 7.55L10 15.1l-6.47 4.7 2.47-7.55L0 7.55h8.02L10 0z"
+						fill="#d9d9d9"
+					/>
+				</svg>
+			{/each}
+		</div>
 	</div>
 	<!-- InfoPanel/SizeMenu -->
 	<div class="z-20">
-		<SizeOption id="size" menuItems={sizeStock} />
+		<SizeOption id="size" menuItems={sizeStock} bind:selectedSize />
 	</div>
 	<!-- InfoPanel/Quantity -->
 	<div class="z-10">
-		<QuantityOption id="quantity" menuItems={quantitys} />
+		<QuantityOption id="quantity" menuItems={quantitys} bind:selectedQuantity />
 	</div>
 	<!-- InfoPanel/AddtoCartButton-->
-	<div class="flex flex-row place-content-center w-full pt-[20px] pb-[30px]">
-		<RightArrowButton text="Add To Cart" scale="0.9" />
+	<div class="flex flex-row justify-center content-start w-full pt-[20px]">
+		<RightArrowButton buttonType="Add To Cart" scale="0.9" on:handleOnClick />
 	</div>
 </div>
