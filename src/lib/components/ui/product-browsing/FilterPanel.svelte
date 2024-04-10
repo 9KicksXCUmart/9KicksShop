ce0872a5ed08e462406b21355dc95196132bfc35
locolin1204 <locolin99@gmail.com>
2024-04-11 01:34:40 +0800

    feat: complete basic browsing filtering

--- src/lib/components/ui/product-browsing/FilterPanel.svelte
+++ src/lib/components/ui/product-browsing/FilterPanel.svelte
@@ -1,51 +1,13 @@
-<script>
-	let genders = [
-		{ gender: 'Men', check: false },
-		{ gender: 'Women', check: false },
-		{ gender: 'Kid', check: false }
-	];
+<script lang="ts">
+	import Input from '$lib/components/ui/input/input.svelte';
 
-	let sizes = [
-		{ size: 1, check: false },
-		{ size: 1.5, check: false },
-		{ size: 2, check: false },
-		{ size: 2.5, check: false },
-		{ size: 3, check: false },
-		{ size: 3.5, check: false },
-		{ size: 4, check: false },
-		{ size: 4.5, check: false },
-		{ size: 5, check: false },
-		{ size: 5.5, check: false },
-		{ size: 6, check: false },
-		{ size: 6.5, check: false },
-		{ size: 7, check: false },
-		{ size: 7.5, check: false },
-		{ size: 8, check: false },
-		{ size: 8.5, check: false },
-		{ size: 9, check: false },
-		{ size: 9.5, check: false },
-		{ size: 10, check: false },
-		{ size: 10.5, check: false },
-		{ size: 11, check: false },
-		{ size: 12, check: false },
-		{ size: 13, check: false },
-		{ size: 14, check: false }
-	];
-
-	let prices = [
-		{ price: 'Under $100', check: false },
-		{ price: '$100 - $200', check: false },
-		{ price: '$200 - $300', check: false },
-		{ price: '$300 - $400', check: false },
-		{ price: '$400 - $500', check: false },
-		{ price: 'Over $500', check: false }
-	];
-
-	let brands = [
-		{ brand: 'Nike', check: false },
-		{ brand: 'Adidas', check: false },
-		{ brand: 'New Balance', check: false }
-	];
+	export let genders;
+	export let sizes;
+	export let prices;
+	export let minPrice: string;
+	export let maxPrice: string;
+	export let brands;
+	export let filterProducts = () => {};
 </script>
 
 <div class="flex flex-col h-fit w-[250px] space-y-[20px]">
@@ -56,8 +18,9 @@
 			<label class="flex items-center px-[6px] py-[5px]">
 				<input
 					type="checkbox"
-					class="appearance-none w-[20px] h-[18px] border-2 border-[#ececec] checked:bg-black"
+					class="appearance-none w-[20px] h-[20px] border-2 border-[#ececec] checked:bg-[#ececec]"
 					bind:checked={check}
+					on:change={filterProducts}
 				/>
 				<span class="px-[6px] font-xs"> {brand} </span>
 			</label>
@@ -71,8 +34,9 @@
 			<label class="flex items-center px-[6px] py-[5px]">
 				<input
 					type="checkbox"
-					class="appearance-none w-[20px] h-[18px] border-2 border-[#ececec] checked:bg-black"
+					class="appearance-none w-[20px] h-[20px] border-2 border-[#ececec] checked:bg-[#ececec]"
 					bind:checked={check}
+					on:change={filterProducts}
 				/>
 				<span class="px-[6px] font-xs"> {gender} </span>
 			</label>
@@ -84,12 +48,10 @@
 		<span class="text-2xl font-bold py-[5px] pb-[6px]"> Size </span>
 		<div class="grid grid-cols-4 gap-y-[15px] gap-x-[10px] place-items-center">
 			{#each sizes as { size, check }}
-				<button>
-					<div
-						class="group w-[45px] h-[36px] border border-[#ececec] bg-[#ffffff] transition duration-150 hover:bg-[#000000]"
-					>
-						<span class="font-xs transition duration-150 group-hover:text-white">{size}</span>
-					</div>
+				<button
+					class="group flex items-center justify-center w-[45px] h-[45px] border border-[#ececec] bg-[#ffffff] transition duration-150 hover:bg-gray-100"
+				>
+					<span class="font-xs transition duration-150">{size}</span>
 				</button>
 			{/each}
 		</div>
@@ -98,15 +60,16 @@
 	<!-- Price -->
 	<div class="flex flex-col">
 		<span class="text-2xl font-bold py-[5px]"> Price </span>
-		{#each prices as { price, check }}
-			<label class="flex items-center px-[6px] py-[5px]">
-				<input
-					type="checkbox"
-					class="appearance-none w-[20px] h-[18px] border-2 border-[#ececec] checked:bg-black"
-					bind:checked={check}
-				/>
-				<span class="px-[6px] font-xs"> {price} </span>
-			</label>
-		{/each}
+		<div class="flex flex-row gap-3">
+			<div class="flex flex-col items-center">
+				<Input class="bg-gray-100 box-border h-8" bind:value={minPrice} />
+				<div class="text-sm">Min Price</div>
+			</div>
+			<div>-</div>
+			<div class="flex flex-col items-center">
+				<Input class="bg-gray-100 box-border h-8" bind:value={maxPrice} />
+				<div class="text-sm">Max Price</div>
+			</div>
+		</div>
 	</div>
 </div>
--- src/routes/browsing/+page.svelte
+++ src/routes/browsing/+page.svelte
@@ -1,9 +1,13 @@
-<script>
+<script lang="ts">
 	import PageHeader from '$lib/components/ui/banner/PageHeader.svelte';
 	import SortOption from '$lib/components/ui/dropdown/SortOption.svelte';
 	import FilterPanel from '$lib/components/ui/product-browsing/FilterPanel.svelte';
 	import ProductImage from '$lib/client/images/dunklow_bw_preview.png';
 	import ProductPreviewItem from '$lib/components/ui/product/PreviewItem.svelte';
+	import { onMount } from 'svelte';
+	import { PUBLIC_GO_BACKEND_URL } from '$env/static/public';
+	import LoadingCircle from '$lib/components/ui/loading/LoadingCircle.svelte';
+	import { goto } from '$app/navigation';
 
 	const menuItems = [
 		'Alphabetical (A to Z)',
@@ -12,16 +16,139 @@
 		'Rating (Low to High)',
 		'Rating (High to Low)'
 	];
+	let genders = [
+		{ gender: "Men's", check: false },
+		{ gender: "Women's", check: false },
+		{ gender: "Kid's", check: false }
+	];
 
-	const products = Array(16).fill({
-		productImage: ProductImage,
-		productName: 'Dunk Low B/W',
-		gender: "Men's",
-		price: 'US$115',
-		rating: 4,
-		ratingCount: '84',
-		scale: '1.05'
+	let sizes = [
+		{ size: 5, check: false },
+		{ size: 5.5, check: false },
+		{ size: 6, check: false },
+		{ size: 6.5, check: false },
+		{ size: 7, check: false },
+		{ size: 7.5, check: false },
+		{ size: 8, check: false },
+		{ size: 8.5, check: false },
+		{ size: 9, check: false },
+		{ size: 9.5, check: false },
+		{ size: 10, check: false },
+		{ size: 10.5, check: false },
+		{ size: 11, check: false },
+		{ size: 11.5, check: false },
+		{ size: 12, check: false },
+		{ size: 12.5, check: false },
+		{ size: 13, check: false },
+		{ size: 13.5, check: false },
+		{ size: 14, check: false },
+		{ size: 14.5, check: false }
+	];
+
+	let prices = [
+		{ price: 'Under $100', check: false },
+		{ price: '$100 - $200', check: false },
+		{ price: '$200 - $300', check: false },
+		{ price: '$300 - $400', check: false },
+		{ price: '$400 - $500', check: false },
+		{ price: 'Over $500', check: false }
+	];
+
+	let brands = [
+		{ brand: 'Nike', check: false },
+		{ brand: 'Adidas', check: false },
+		{ brand: 'New Balance', check: false }
+	];
+
+	let minPrice: string;
+	let maxPrice: string;
+
+	let products = [];
+	let isLoading = true;
+
+	onMount(async () => {
+		await handleFilterProducts();
 	});
+
+	async function handleFilterProducts() {
+		isLoading = true;
+		const result = await searchProducts();
+		if (result.amount === 0) {
+			products = [];
+		} else {
+			products = result.products;
+		}
+		isLoading = false;
+	}
+
+	function checkFilter() {
+		let filter: any = {};
+
+		filter.pageNum = 1;
+
+		let selectedBrand: string = '';
+		brands.forEach((brand) => {
+			if (brand.check === true) {
+				selectedBrand = brand.brand;
+			}
+		});
+		if (selectedBrand !== '') filter.brand = selectedBrand;
+
+		let selectedGender: string = '';
+		genders.forEach((gender) => {
+			if (gender.check === true) {
+				selectedGender = gender.gender;
+			}
+		});
+		if (selectedGender !== '') filter.category = selectedGender;
+
+		let selectedMinPrice: string = '';
+		if (minPrice !== '') {
+			selectedMinPrice = minPrice;
+		} else {
+			selectedMinPrice = '';
+		}
+
+		let selectedMaxPrice: string = '';
+		if (maxPrice !== '') {
+			selectedMaxPrice = minPrice;
+		} else {
+			selectedMaxPrice = '';
+		}
+
+		// return {
+		// 	selectedBrand,
+		// 	selectedSize,
+		// 	selectedGender,
+		// 	selectedMinPrice,
+		// 	selectedMaxPrice
+		// };
+		return filter;
+	}
+
+	async function searchProducts() {
+		const filter = checkFilter();
+
+		console.log(filter);
+		const url = new URL(`${PUBLIC_GO_BACKEND_URL}/v1/products`);
+		url.search = new URLSearchParams(filter).toString();
+
+		console.log(`url ${url.search}`);
+		const response = await fetch(url, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const result = await response.json();
+		console.log(result);
+		return result.data;
+	}
+
+	function routeToProductDetail(productId: string) {
+		console.log('clicked');
+		goto(`/browsing/${productId}`);
+	}
 </script>
 
 <div class="flex flex-col items-center w-full h-fit">
@@ -34,22 +161,47 @@
 
 		<div class="flex flex-row items-start w-full h-fit px-[17%] py-[1%] space-x-[6%]">
 			<!-- Product Filters-->
-			<FilterPanel />
+			<FilterPanel
+				{sizes}
+				{brands}
+				{prices}
+				{genders}
+				{minPrice}
+				{maxPrice}
+				filterProducts={handleFilterProducts}
+			/>
 
 			<!-- Display Products -->
-			<div class="grid grid-cols-4 w-full justify-between gap-y-[20px]">
-				{#each products as { productImage, productName, gender, price, rating, ratingCount, scale }}
-					<ProductPreviewItem
-						{productImage}
-						{productName}
-						{gender}
-						{price}
-						{rating}
-						{ratingCount}
-						{scale}
-					/>
-				{/each}
-			</div>
+			{#if isLoading}
+				<div class="w-full flex flex-col items-center">
+					<LoadingCircle size="60" />
+				</div>
+			{:else if products.length === 0}
+				<div class="w-full text-3xl text-center">No Results</div>
+			{:else}
+				<div class="grid grid-cols-4 w-full justify-between gap-y-[20px]">
+					{#each products as { id, imageUrl, name, category, price }}
+						<button
+							on:click={() => {
+								routeToProductDetail(id);
+							}}
+							class="hover:scale-105 duration-300"
+						>
+							<ProductPreviewItem
+								productImage={imageUrl}
+								productName={name}
+								{category}
+								{price}
+								rating={Math.floor(Math.random() * 5) + 1}
+								ratingCount={Math.floor(Math.random() * 100) + 5}
+								scale="1.05"
+							/>
+						</button>
+						<!--{rating}-->
+						<!--{ratingCount}-->
+					{/each}
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>
