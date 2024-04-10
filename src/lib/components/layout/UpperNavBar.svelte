4d91446dc322ec151504a0296a7ee781edf9f860
locolin1204 <locolin99@gmail.com>
2024-04-11 06:26:10 +0800

    fix: search bar searching

--- src/lib/components/layout/UpperNavBar.svelte
+++ src/lib/components/layout/UpperNavBar.svelte
@@ -69,7 +69,7 @@
 	}
 	function handleSearchKeyword() {
 		searchKeywordStore.set(searchKeyword);
-		console.log($searchKeywordStore);
+		console.log('search keyword', $searchKeywordStore);
 		setTimeout(() => goto('/browsing'), 0);
 	}
 
@@ -93,33 +93,33 @@
 			<div
 				class="relative flex flex-row items-center justify-between text-gray-400 focus-within:text-gray-600"
 			>
-				<button type="submit" class="absolute">
-					<svg
-						class="h-4 w-4 ml-3 fill-current"
-						xmlns="http://www.w3.org/2000/svg"
-						xmlns:xlink="http://www.w3.org/1999/xlink"
-						version="1.1"
-						id="Capa_1"
-						x="0px"
-						y="0px"
-						viewBox="0 0 56.966 56.966"
-						style="enable-background:new 0 0 56.966 56.966;"
-						xml:space="preserve"
-						width="512px"
-						height="512px"
-					>
-						<path
-							d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
-						/>
-					</svg>
-				</button>
-				<Input
-					placeholder="Search"
-					class="pl-10 pr-3 font-semibold placeholder-gray-500 text-black"
-					bind:value={searchKeyword}
-					on:keydown={handleEnterPress}
-					on:blur={handleOnBlur}
-				/>
+				<!--				<button type="submit" class="absolute">-->
+				<!--					<svg-->
+				<!--						class="h-4 w-4 ml-3 fill-current"-->
+				<!--						xmlns="http://www.w3.org/2000/svg"-->
+				<!--						xmlns:xlink="http://www.w3.org/1999/xlink"-->
+				<!--						version="1.1"-->
+				<!--						id="Capa_1"-->
+				<!--						x="0px"-->
+				<!--						y="0px"-->
+				<!--						viewBox="0 0 56.966 56.966"-->
+				<!--						style="enable-background:new 0 0 56.966 56.966;"-->
+				<!--						xml:space="preserve"-->
+				<!--						width="512px"-->
+				<!--						height="512px"-->
+				<!--					>-->
+				<!--						<path-->
+				<!--							d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"-->
+				<!--						/>-->
+				<!--					</svg>-->
+				<!--				</button>-->
+				<!--				<Input-->
+				<!--					placeholder="Search"-->
+				<!--					class="pl-10 pr-3 font-semibold placeholder-gray-500 text-black"-->
+				<!--					bind:value={searchKeyword}-->
+				<!--					on:keydown={handleEnterPress}-->
+				<!--					on:blur={handleOnBlur}-->
+				<!--				/>-->
 			</div>
 		</form>
 		{#if (isLogin && !$loggedOut) || triggerLogin}
